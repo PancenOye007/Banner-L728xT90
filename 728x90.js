cc0ece8196cc4ef6662d3b97b0eea923ebdbb5d2
PancenOye007 <115642214+PancenOye007@users.noreply.github.com>
2023-03-03 10:07:38 +0700

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'a32c114e2000ac94ec30f27cd6cda51b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/a32c114e2000ac94ec30f27cd6cda51b/invoke.js"></scr' + 'ipt>');
